e7c12e639009abd697689fb7336662874eaa5f4b
LeTanPhat <letanphat857@gmail.com>
2021-06-03 19:16:12 +0700

    LeTanPhat edited body component

--- appNew-Vnexpress/src/app/components/body/body.module.ts
+++ appNew-Vnexpress/src/app/components/body/body.module.ts
@@ -2,15 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BodyComponent } from './body.component';
 import {MatCardModule} from '@angular/material/card';
-
-
+import { MatIconModule } from '@angular/material/icon';
 @NgModule({
   declarations: [
     BodyComponent
   ],
   imports: [
     CommonModule,
-    MatCardModule
+    MatCardModule,
+    MatIconModule,
   ]
 })
 export class BodyModule { }

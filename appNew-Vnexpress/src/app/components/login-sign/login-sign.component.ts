aa11c80549ccd3582f7abcbb59da971f07618902
TylerVuNguyen <73806963+nguyenvantruong123asd@users.noreply.github.com>
2021-06-05 01:21:31 +0700

    .
    .

--- appNew-Vnexpress/src/app/components/login-sign/login-sign.component.ts
+++ appNew-Vnexpress/src/app/components/login-sign/login-sign.component.ts
@@ -4,14 +4,18 @@ import { AccountService } from 'src/app/services/account.service';
 @Component({
   selector: 'app-login-sign',
   templateUrl: './login-sign.component.html',
-  styleUrls: ['./login-sign.component.scss']
+  styleUrls: ['./login-sign.component.scss'],
 })
 export class LoginSignComponent implements OnInit {
-  name: string = 'Ly Na';
+  name: string = 'truong';
   password: string = '123';
+  flag:boolean = true
 
+  apply(value:string){
+    this.flag = value == "login"?true:false;
+  }
+  
   constructor(private accountService: AccountService) { }
-
   ngOnInit(): void {
     this.accountService.currentAccount.subscribe(name => this.name = name);
   }

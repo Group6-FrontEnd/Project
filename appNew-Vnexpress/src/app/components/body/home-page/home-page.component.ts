597bd9a1caa3aa58e6d776396455348397d27e80
TylerVuNguyen <73806963+nguyenvantruong123asd@users.noreply.github.com>
2021-09-17 19:26:43 +0700

    cn
    cn

--- appNew-Vnexpress/src/app/components/body/home-page/home-page.component.ts
+++ appNew-Vnexpress/src/app/components/body/home-page/home-page.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { OwlOptions } from 'ngx-owl-carousel-o';
+import { Router } from '@angular/router';
 import { Account } from 'src/app/models/account';
 import { ResponseObject } from 'src/app/models/responseObject';
 import { AccountService } from 'src/app/services/account/account.service';
 import { Rss2jsonService } from 'src/app/services/rss2json/rss2json.service';
+import { HistoryService } from 'src/app/services/history/history.service';
+import { SavedNewsService } from 'src/app/services/saved-news/saved-news.service';
 
 @Component({
   selector: 'app-home-page',
@@ -11,6 +14,11 @@ import { Rss2jsonService } from 'src/app/services/rss2json/rss2json.service';
   styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent implements OnInit {
+  countRead = 1;
+  view = 'Card';
+  sortBy = 'Date';
+  link_rss = '';
+  loadNumber = 0;
   customOptions: OwlOptions = {
     loop: true,
     autoplay: true,
@@ -96,6 +104,33 @@ export class HomePageComponent implements OnInit {
       }
     ]
   };
+  responseObjectData: ResponseObject = {
+    status: '',
+    feed: {
+      url: '',
+      title: '',
+      link: '',
+      author: '',
+      description: '',
+      image: '',
+    },
+    items: [
+      {
+        title: '',
+        pubDate: '',
+        link: '',
+        guid: '',
+        author: '',
+        thumbnail: '',
+        description: '',
+        content: '',
+        enclosure: {
+        },
+        categories: [
+        ]
+      }
+    ]
+  };
 
   getJson(url: string): void {
     this.rss2jsonService.getJson(url).subscribe(content => {
@@ -106,6 +141,13 @@ export class HomePageComponent implements OnInit {
       console.log(this.responseObjectLatestNews);
     });
   }
+constructor(private rss2jsonService: Rss2jsonService,
+    private router: Router,
+    private historyService: HistoryService,
+    private savedService: SavedNewsService,
+    private accountService: AccountService
+  ) {
+  }
   getJson1(url: string): void {
     this.rss2jsonService.getJson(url).subscribe(content => {
       this.responseObject = content;
@@ -115,16 +157,29 @@ export class HomePageComponent implements OnInit {
       console.log(this.responseObject);
     });
   }
+  read(id: any) {
+    console.log('Đọc: ' + id.title);
+    let index = this.responseObject.items.indexOf(id);
+    let indexData = this.responseObjectData.items.indexOf(id);
 
+    if (index != -1) {
+      this.countRead++;
+
+      if (id.description.includes('read')) {
+        this.historyService.histories[0].id = this.countRead.toString();
+        this.savedService.count= this.countRead;
+      } else {     
+        this.responseObject.items[index].description += ' read';
+        this.responseObjectData.items[indexData].description += ' read';
+        this.historyService.getRss(id);
+      }
+    }
+  }
   getContent(origin: string): string {
     return origin.substring(origin.lastIndexOf('>') + 1);
   }
-
-  constructor(private rss2jsonService: Rss2jsonService) { }
-
   ngOnInit(): void {
     this.getJson(this.link_url[0]);
     this.getJson1(this.link_url[1]);
   }
-
 }
